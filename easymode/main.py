c177a784e0eb42278a0f62034ca919f333376fa6
Yemeskabeba <36342713+Yemte@users.noreply.github.com>
2020-03-22 16:22:33 +0200

    try exception problem solved while loop created and works fine the player can quit the game at any time The game is ready and works like it should be

--- easymode/main.py
+++ easymode/main.py
@@ -20,18 +20,14 @@ print("\t\t2: Choose an action")
 print("\t\t3: at the beginning the hp and mp are at maximum point.")
 print("\t\t4: hp deacreases when attacked.")
 print("\t\t5: game will end when hp equals zero")
+print("\t\t6: You can end quit the game by typinng upper case Q ")
 print("\t ---- HAVE FUN ----")
+
 print("************************************")
 
 player_name= input("what is your name:")
 player = person(player_name,200,50,15)
 enemy = person('enemy',100,30,11)
-# print(player.name.upper(),end=(':'))
-# print(player.maxhp)
-# print("\t ",player.maxmp)
-# print(enemy.name.upper(),end=(':'))
-# print(enemy.hp/enemy.maxhp)
-# print("\t ",enemy.maxmp)
 player.get_status()
 enemy.get_status()
 
@@ -39,16 +35,74 @@ print("************************************* ")
 print("************************************* ")
 
 player.choose_action()
+playing = True
+while playing:
+    try:
+        player_action = int(input("chose an action:"))
+        if player_action == 1:
+                player.action[0]
+                damage = player.generate_damage()
+                tak_damage = enemy.take_damage(damage)
+                print(damage, " damages has been caused to the enemy")
 
-player_action = int(input("chose an action:"))
-try:
-    if player_action==1:
-        player.action[0]
-        damage= enemy.generate_damage()
-        tak_damage = enemy.take_damage(damage)
-        print(tak_damage," damages has been caused to the player")
-    elif player_action >1:
-        print("Sorry you have only one action to choose,choose number 1")
-except:
-    print(player_action,"is not a number please enter a valid number")
+                print("****** NOW IT\'S  THE ENEMY'S TURN ******")
+                dam = enemy.generate_damage()
+                tak_dam = player.take_damage(dam)
+                print(dam , " damages has been caused to the player")
 
+        elif player_action > 1:
+            print("Sorry you have only one action to choose,choose number 1")
+            player_action = int(input("chose an action:"))
+            if player_action == 1:
+                player.action[0]
+                damage = player.generate_damage()
+                tak_damage = enemy.take_damage(damage)
+                print(damage, " damages has been caused to the enemy")
+                print("****** NOW IT\'S  THE ENEMY'S TURN ******")
+                dam = enemy.generate_damage()
+                tak_dam = player.take_damage(dam)
+                print(dam, " damages has been caused to the player")
+
+    except ValueError:
+        print(" please enter a valid number")
+        player_action = int(input("chose an action:"))
+        if player_action == 1:
+            player.action[0]
+            damage = enemy.generate_damage()
+            tak_damage = enemy.take_damage(damage)
+            print(damage, " damages has been caused to the enemy")
+            print("****** NOW IT\'S  THE ENEMY'S TURN ******")
+            dam = player.generate_damage()
+            tak_dam = player.take_damage(dam)
+            print(dam, " damages has been caused to the player")
+
+        elif player_action > 1:
+            print("Sorry you have only one action to choose,choose number 1")
+            player_action = int(input("chose an action:"))
+            if player_action == 1:
+                player.action[0]
+                damage = enemy.generate_damage()
+                tak_damage = enemy.take_damage(damage)
+                print(damage, " damages has been caused to the enemy")
+                print("****** NOW IT\'S  THE ENEMY'S TURN ******")
+                dam = player.generate_damage()
+                tak_dam = player.take_damage(dam)
+                print(dam, " damages has been caused to the player")
+    except :
+        print(" opss Something went wrong")
+    print("****** BELOW IS THE CURRENT STATUS FOR BOTH ******")
+    player.get_status()
+    enemy.get_status()
+    # print("****** NOW IT\'S  YOUR TURN  PRESS ENETER******")
+    X = input()
+    if player.hp==0:
+        playing = False
+        print("YOU LOST!!")
+
+    elif enemy.hp==0:
+        playing = False
+        print("YOU WINNNNNNNN")
+
+    elif X=='Q':
+        playing = False
+        print("YOU QUIT")
